6b49cec35072077065848850be2f6af220aaa63f
wsz <1175690167@qq.com>
2025-03-18 01:48:15 +0800

    更新：测试数据

--- core/generate.sh
+++ core/generate.sh
@@ -1,1 +1,1 @@
-python3 generate_test_case.py --num-actions 10 --replaceable-range 1,3 --non-replaceable-range 1,3 --replacement-range 1,3 --time-limit 5 --solution-count 3
+python3 generate_test_case.py --num-actions 100 --replaceable-range 1,3 --non-replaceable-range 1,3 --replacement-range 1,3 --time-limit 50 --solution-count 3
